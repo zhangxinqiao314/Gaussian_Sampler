c2747e6b691ac4e045b6bf21f06995a17d0d6da9
zhangxinqiao314 <xz498@drexel.edu>
2024-11-14 21:27:21 +0000

    Affine_Transform first pass

---
+++ Gaussian_Sampler/models/affine_AE.py
@@ -0,0 +1,369 @@
+from m3_learning.nn.STEM_AE import STEM_AE
+import numpy as np
+import torch
+from torch.utils.data import Sampler
+from m3_learning.nn.Regularization.Regularizers import ContrastiveLoss, DivergenceLoss
+from tqdm import tqdm
+import torch.nn.functional as F
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from m3_learning.util.file_IO import make_folder
+
+
+class Affine_Transform(nn.Module):
+    def __init__(self,
+                 device,
+                 scale = True,
+                 shear = True,
+                 rotation = True,
+                 translation = True,
+                 Symmetric = True,
+                 mask_intensity = True,
+                 scale_limit = 0.05,
+                 shear_limit = 0.1,
+                 rotation_limit = 0.1,
+                 trans_limit = 0.15,
+                 adj_mask_para=0
+                 ):
+        super(Affine_Transform,self).__init__()
+        self.scale = scale
+        self.shear = shear
+        self.rotation = rotation
+        self.translation = translation
+        self.Symmetric = Symmetric
+        self.scale_limit = scale_limit
+        self.shear_limit = shear_limit
+        self.rotation_limit = rotation_limit
+        self.trans_limit = trans_limit
+        self.adj_mask_para = adj_mask_para
+        self.mask_intensity = mask_intensity
+        self.device = device
+        self.count = 0
+
+    def forward(self,out,rotate_value = None):
+
+        if self.scale:
+            scale_1 = self.scale_limit*nn.Tanh()(out[:,self.count])+1
+            scale_2 = self.scale_limit*nn.Tanh()(out[:,self.count+1])+1
+            self.count +=2
+        else:
+            scale_1 = torch.ones([out.shape[0]]).to(self.device)
+            scale_2 = torch.ones([out.shape[0]]).to(self.device)
+
+        if self.rotation:
+            if rotate_value!=None:
+                # use large mask no need to limit to too small range
+                rotate = rotate_value.reshape(out[:,self.count].shape) + self.rotation_limit*nn.Tanh()(out[:,self.count])
+                self.count+=1
+            else:
+                rotate = nn.ReLU()(out[:,self.count])
+                self.count+=1
+        else:
+            rotate = torch.zeros([out.shape[0]]).to(self.device)
+
+        if self.shear:
+            if self.Symmetric:
+                shear_1 = self.shear_limit*nn.Tanh()(out[:,self.count])
+                shear_2 = shear_1
+                self.count+=1
+            else:
+                shear_1 = self.shear_limit*nn.Tanh()(out[:,self.count])
+                shear_2 = self.shear_limit*nn.Tanh()(out[:,self.count+1])
+                self.count+=2
+        else:
+            shear_1 = torch.zeros([out.shape[0]]).to(self.device)
+            shear_2 = torch.zeros([out.shape[0]]).to(self.device)
+        # usually the 4d-stem has symetric shear value, we make xy=yx, that's the reason we don't need shear2
+
+        if self.translation:
+            trans_1 = self.trans_limit*nn.Tanh()(out[:,self.count])
+            trans_2 = self.trans_limit*nn.Tanh()(out[:,self.count+1])
+            self.count +=2
+        else:
+            trans_1 = torch.zeros([out.shape[0]]).to(self.device)
+            trans_2 = torch.zeros([out.shape[0]]).to(self.device)
+  
+        if self.mask_intensity:
+            mask_parameter = self.adj_mask_para*nn.Tanh()(out[:,self.embedding_size:self.embedding_size+1])+1
+        else:
+            # this project doesn't need mask parameter to adjust value intensity in mask region, so we make it 1 here.
+            mask_parameter = torch.ones([out.shape[0]])
+
+        self.count = 0
+ 
+        a_1 = torch.cos(rotate)
+        a_2 = torch.sin(rotate)
+        a_4 = torch.ones([out.shape[0]]).to(self.device)
+        a_5 = torch.zeros([out.shape[0]]).to(self.device)
+
+       # combine shear and strain together
+        c1 = torch.stack((scale_1,shear_1), dim=1).squeeze()
+        c2 = torch.stack((shear_2,scale_2), dim=1).squeeze()
+        c3 = torch.stack((a_5,a_5), dim=1).squeeze()
+        scaler_shear = torch.stack((c1, c2, c3), dim=2)
+
+        # Add the rotation after the shear and strain
+        b1 = torch.stack((a_1,a_2), dim=1).squeeze()
+        b2 = torch.stack((-a_2,a_1), dim=1).squeeze()
+        b3 = torch.stack((a_5,a_5), dim=1).squeeze()
+        rotation = torch.stack((b1, b2, b3), dim=2)
+
+        d1 = torch.stack((a_4,a_5), dim=1).squeeze()
+        d2 = torch.stack((a_5,a_4), dim=1).squeeze()
+        d3 = torch.stack((trans_1,trans_2), dim=1).squeeze()
+        translation = torch.stack((d1, d2, d3), dim=2)
+
+        return scaler_shear, rotation, translation, mask_parameter
+
+
+class Affine_AE_2D(nn.Module):
+    def __init__(self,
+                 device='cuda',
+                 affine_encoder = STEM_AE.Encoder,
+                 affine_encoder_kwargs = { 'original_step_size': [100,100], 
+                                           'pooling_list': [], 
+                                           'embedding_size': 6, 
+                                           'conv_size': 128,
+                                           'kernel_size': 3,},
+                 affine_model = Affine_Transform,
+                 affine_kwargs = {  "scale": True,
+                                    "shear": True,
+                                    "rotation": True,
+                                    "translation": True,
+                                    "Symmetric": True,
+                                    "mask_intensity": True,
+                                    "scale_limit": 0.05,
+                                    "shear_limit": 0.1,
+                                    "rotation_limit": 0.1,
+                                    "trans_limit": 0.15,
+                                    "adj_mask_para": 0  },
+                 
+                
+                 
+                 ):
+
+        super(Affine_AE_2D, self).__init__(affine_kwargs=affine_kwargs)
+        
+        self.device = device
+        
+        for key, value in affine_encoder_kwargs.items():
+            setattr(self, key, value)
+        self.encoder = affine_encoder(**affine_encoder_kwargs)
+        
+        affine_kwargs['device'] = device
+        for key, value in affine_kwargs.items():
+            setattr(self, key, value)
+        self.affine_model = affine_model(**affine_kwargs)
+        
+        
+            
+            
+        self.affine = Affine_Transform(self.device, scale = True, shear = True, rotation = True, translation = True, Symmetric = True, mask_intensity = True)
+
+
+        self.decoder = nn.Sequential(
+            nn.Linear(self.embedding_size, 2*16*16),
+            nn.ReLU(),
+            nn.Unflatten(1, (2, 16, 16)),
+            nn.ConvTranspose2d(2, 4, 3, stride=2, padding=1, output_padding=1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(4, 8, 3, stride=2, padding=1, output_padding=1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(8, 16, 3, stride=2, padding=1, output_padding=1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(16, 1, 3, stride=2, padding=1, output_padding=1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        emb_affine = self.encoder(x)
+        scaler_shear, rotation, translation, mask_parameter = self.affine(x)
+        x = self.decoder(x)
+        return x, scaler_shear, rotation, translation, mask_parameter
+
+    def loss_function(self,
+                      train
+
+class Averaging_Loss_AE(STEM_AE.ConvAutoencoder):
+    def __init__(self, sampler, sampler_kwargs, collate_fn, *args, **kwargs):
+        super(Averaging_Loss_AE, self).__init__(*args, **kwargs)
+        self.sampler = sampler(**sampler_kwargs)
+       
+    def Train(self,
+              data,
+              max_learning_rate=1e-4,
+              coef_1=0,
+              coef_2=0,
+              coef_3=0,
+              seed=12,
+              epochs=100,
+              with_scheduler=True,
+              ln_parm=1,
+              epoch_=None,
+              folder_path='./',
+              batch_size=32,
+              best_train_loss=None,
+              dataloader_init = None):
+        """function that trains the model
+
+        Args:
+            data (torch.tensor): data to train the model
+            max_learning_rate (float, optional): sets the max learning rate for the learning rate cycler. Defaults to 1e-4.
+            coef_1 (float, optional): hyperparameter for ln loss. Defaults to 0.
+            coef_2 (float, optional): hyperparameter for contrastive loss. Defaults to 0.
+            coef_3 (float, optional): hyperparameter for divergency loss. Defaults to 0.
+            seed (int, optional): sets the random seed. Defaults to 12.
+            epochs (int, optional): number of epochs to train. Defaults to 100.
+            with_scheduler (bool, optional): sets if you should use the learning rate cycler. Defaults to True.
+            ln_parm (int, optional): order of the Ln regularization. Defaults to 1.
+            epoch_ (int, optional): current epoch for continuing training. Defaults to None.
+            folder_path (str, optional): path where to save the weights. Defaults to './'.
+            batch_size (int, optional): sets the batch size for training. Defaults to 32.
+            best_train_loss (float, optional): current loss value to determine if you should save the value. Defaults to None.
+        """
+
+        make_folder(folder_path)
+
+        # set seed
+        torch.manual_seed(seed)
+        
+        # builds the dataloader
+        if dataloader_init is None:
+            self.DataLoader_ = DataLoader(
+                data.reshape(-1, 256, 256), batch_size=batch_size, shuffle=True)
+        else:
+            self.DataLoader_ = DataLoader(data, **dataloader_init)
+
+        # option to use the learning rate scheduler
+        if with_scheduler:
+            scheduler = torch.optim.lr_scheduler.CyclicLR(
+                self.optimizer, base_lr=self.learning_rate, max_lr=max_learning_rate, step_size_up=15, cycle_momentum=False)
+        else:
+            scheduler = None
+
+        # set the number of epochs
+        N_EPOCHS = epochs
+
+        # initializes the best train loss
+        if best_train_loss == None:
+            best_train_loss = float('inf')
+
+        # initialize the epoch counter
+        if epoch_ is None:
+            self.start_epoch = 0
+        else:
+            self.start_epoch = epoch_+1
+
+        # training loop
+        for epoch in range(self.start_epoch, N_EPOCHS):
+            train = self.loss_function(
+                self.DataLoader_, coef_1, coef_2, coef_3, ln_parm)
+            train_loss = train
+            train_loss /= len(self.DataLoader_)
+            print(
+                f'Epoch: {epoch:03d}/{N_EPOCHS:03d} | Train Loss: {train_loss:.4f}')
+            print('.............................')
+
+          #  schedular.step()
+            if best_train_loss > train_loss:
+                best_train_loss = train_loss
+                checkpoint = {
+                    "net": self.autoencoder.state_dict(),
+                    'optimizer': self.optimizer.state_dict(),
+                    "epoch": epoch,
+                    "encoder": self.encoder.state_dict(),
+                    'decoder': self.decoder.state_dict(),
+                }
+                if epoch >= 0:
+                    lr_ = format(self.optimizer.param_groups[0]['lr'], '.5f')
+                    file_path = folder_path + '/Weight_' +\
+                        f'epoch:{epoch:04d}_l1coef:{coef_1:.4f}'+'_lr:'+lr_ +\
+                        f'_trainloss:{train_loss:.4f}.pkl'
+                    torch.save(checkpoint, file_path)
+
+            if scheduler is not None:
+                scheduler.step()
+        
+    def loss_function(self,
+                      train_iterator,
+                      coef=0,
+                      coef1=0,
+                      coef2=0,
+                      ln_parm=1,
+                      binning=False,
+                      weight_by_distance=False):
+        """computes the loss function for the training
+
+        Args:
+            train_iterator (torch.Dataloader): dataloader for the training
+            coef (float, optional): Ln hyperparameter. Defaults to 0.
+            coef1 (float, optional): hyperparameter for contrastive loss. Defaults to 0.
+            coef2 (float, optional): hyperparameter for divergence loss. Defaults to 0.
+            ln_parm (float, optional): order of the regularization. Defaults to 1.
+            beta (float, optional): beta value for VAE. Defaults to None.
+
+        Returns:
+            _type_: _description_
+        """
+
+        # set the train mode
+        self.autoencoder.train()
+
+        # loss of the epoch
+        train_loss = 0
+        con_l = ContrastiveLoss(coef1).to(self.device)
+
+        for x in tqdm(train_iterator, leave=True, total=len(train_iterator)):
+
+            x = x.to(self.device, dtype=torch.float)
+            maxi_ = DivergenceLoss(x.shape[0], coef2).to(self.device)
+            
+            
+            # update the gradients to zero
+            self.optimizer.zero_grad()
+
+            embedding = self.encoder(x)
+            reg_loss_1 = coef * torch.norm(embedding, ln_parm).to(self.device)/x.shape[0]
+            if reg_loss_1 == 0: reg_loss_1 = 0.5
+            predicted_x = self.decoder(embedding)
+            contras_loss = con_l(embedding)
+            maxi_loss = maxi_(embedding)
+            
+            # i, idx, x are lists. Each list is the gaussian samples from a batch
+            if binning:
+                # x = list(torch.split(x, self.dataloader_sampler.num_neighbors)) # Split the batch into groups based on the number of neighbors
+                # predicted_x = list(torch.split(predicted_x, self.dataloader_sampler.num_neighbors))
+                
+                x = self.sampler._split_list(x) # Split the batch into groups based on the number of neighbors
+                predicted_x = self.sampler._split_list(predicted_x)
+                
+                if weight_by_distance:
+                    idx = self.sampler._split_list(idx, self.dataloader_sampler.num_neighbors) # Split the indices into groups based on the number of neighbors
+                    weight_list = []
+                    for i_, sample_group in enumerate(idx):
+                        coords = [(ind % self.shape[0], int(ind /self.shape[0])) for ind in sample_group] # Calculate the coordinates relative to the first point in the group
+                        weights = torch.tensor([1] +[1 /(1 +((coords[0][0]-coord[0])**2 +(coords[0][1]-coord[1])**2)**0.5) for coord in coords[1:]], # Calculate weights based on the Euclidean distance to the first point
+                                                device = self.device) 
+                        x[i_] = x[i_] *weights.unsqueeze(-1).unsqueeze(-1)
+                        predicted_x[i_] = predicted_x[i_]*weights.unsqueeze(-1).unsqueeze(-1)  
+                        weight_list.append(weights)
+                    weight_sums = torch.stack([weight_.sum(dim=0) for weight_ in weight_list]).unsqueeze(-1).unsqueeze(-1) # Sum the weights in each group and stack them into a new batch
+                    x = torch.stack([x_.sum(dim=0) for x_ in x])/weight_sums # Sum the tensors in each group and stack them into a new batch
+                    predicted_x = torch.stack([x_.sum(dim=0) for x_ in predicted_x])/weight_sums
+                else:        
+                    x = torch.stack([x_.mean(dim=0) for x_ in x]) # Sum the tensors in each group and stack them into a new batch
+                    predicted_x = torch.stack([x_.mean(dim=0) for x_ in predicted_x])
+                    # print('')
+
+            # reconstruction loss
+            loss = F.mse_loss(x, predicted_x, reduction='mean')
+            
+            loss = loss + reg_loss_1 + contras_loss - maxi_loss
+
+            # backward pass
+            train_loss += loss.item()
+            loss.backward()
+            # update the weights
+            self.optimizer.step()
+
+        return train_loss
