648ddd853492dd4cf4ef56d4118a310d04cc4275
zhangxinqiao314 <zhangxinqiao314@gmail.com>
2025-02-25 22:05:22 +0000

    Refactor loss function with modular components and improved binning logic

--- Gaussian_Sampler/models/pv_fitter.py
+++ Gaussian_Sampler/models/pv_fitter.py
@@ -1,15 +1,18 @@
 from random import shuffle
 import sys
+import os
 sys.path.append('/home/m3learning/Northwestern/M3Learning-Util/src')
 from STEM_EELS_Curve_Fitting.Data import Sampler
 from m3util.utils.IO import make_folder
+from m3util.ml.regularization import Weighted_LN_loss, ContrastiveLoss, DivergenceLoss, Sparse_Max_Loss
 
 from AutoPhysLearn.src.autophyslearn.spectroscopic.nn import Multiscale1DFitter, Conv_Block, FC_Block, block_factory
 
 from ..data.custom_sampler import Gaussian_Sampler, custom_collate_fn
 
 import torch
-from torch import nn
+from torch import nn, optim
+from torch.nn import functional as F
 from torch.utils.data import DataLoader
 
 from datetime import date
@@ -24,7 +27,7 @@ class Fitter_AE:
                  device='cuda:0',
                  flatten_from = 1,
                  encoder = Multiscale1DFitter,
-                 encoder_params = { "model_block_dict": { 
+                 encoder_params = { "model_block_dict": { # factory wrapper for blocks
                     "hidden_x1": block_factory(Conv_Block)(output_channels_list=[8,6,4], 
                                                            kernel_size_list=[7,7,5], 
                                                            pool_list=[64], 
@@ -48,11 +51,21 @@ class Fitter_AE:
                                 x_data = dset,
                                 input_channels = dset.shape[1],
                                 num_params = num_params,
-                                device=device,**encoder_params)
-        
+                                device=device,
+                                **encoder_params
+                                ).to(self.device).type(torch.float32)
+        self.optimizer = optim.Adam(
+            self.Fitter.parameters(), lr=self.learning_rate
+        )
         self._dataloader = None
         self._dataloader_sampler = None
         
+        self.start_epoch = 0
+        self.best_train_loss = float('inf')
+        self.checkpoint = None
+        self.folder = None
+        self.optimizer = None
+        self.scheduler = None
         
     @property
     def dataloader_sampler(self):
@@ -76,7 +89,6 @@ class Fitter_AE:
         return self._dataloader
     def configure_dataloader(self, **kwargs):
         '''Set the dataloader for the fitter
-
         Args:
             batch_size (int, optional): Number of samples per batch. Defaults to 32.
             sampler (Sampler, optional): Defines the strategy to draw samples from the dataset. Defaults to None.
@@ -94,8 +106,36 @@ class Fitter_AE:
         else:
             self._dataloader = DataLoader(self.dset, batch_size=batch_size, sampler=sampler, collate_fn=collate_fn, shuffle=shuffle)
 
+        
+    @property
+    def checkpoint_folder(self): return self._checkpoint_folder
+    @checkpoint_folder.setter
+    def checkpoint_folder(self,value): self._checkpoint_folder = value
     
-    def train(self, seed=42):
+    @property 
+    def checkpoint_file(self): return self._checkpoint_file
+    
+    @property
+    def check(self): return self._check
+    
+    @property
+    def checkpoint(self): return self._checkpoint
+    @checkpoint.setter
+    def checkpoint(self, value):
+        self._checkpoint = value
+        try:
+            checkpoint_folder,checkpoint_file = os.path.split(self._checkpoint)
+            self._checkpoint_file = checkpoint_file
+            self._check = checkpoint_file.split('.pkl')[0]
+            self.checkpoint_folder = checkpoint_folder
+        except:
+            self.check = None
+            self.checkpoint_folder = None
+            self.checkpoint_file = None
+            
+    
+    def train(self, seed=42, epochs=100, binning=True, weight_by_distance=True):
+        
         today = date.today()
         save_date=today.strftime('(%Y-%m-%d)')
         make_folder(self.folder)
@@ -103,39 +143,11 @@ class Fitter_AE:
         # set seed
         torch.manual_seed(seed)
 
-
-        # initializes the best train loss
-        if best_train_loss == None:
-            best_train_loss = float('inf')
-
-        # initialize the epoch counter
-        if epoch_ is None:
-            self.start_epoch = 0
-        else:
-            self.start_epoch = epoch_
-
-        if self.wandb_project is not None:  
-            wandb_init['project'] = self.wandb_project
-            wandb.init(**wandb_init) # figure out config later
-            
         # training loop
-        for epoch in range(self.start_epoch, N_EPOCHS):
+        for epoch in range(self.start_epoch, epochs):
             fill_embeddings = False
-            # if save_emb_every is not None and epoch % save_emb_every == 0: # tell loss function to give embedding
-            #     print(f'Epoch: {epoch:03d}/{N_EPOCHS:03d}, getting embedding')
-            #     print('.............................')
-            #     fill_embeddings = self.get_embedding(data, train=True)
 
-
-            loss_dict = self.loss_function( self.DataLoader_,
-                                            coef1=coef_1,
-                                            coef2=coef_2,
-                                            coef3=coef_3,
-                                            coef4=coef_4,
-                                            coef5=coef_5,
-                                            ln_parm=ln_parm,
-                                            fill_embeddings=fill_embeddings,
-                                            minibatch_logging_rate=minibatch_logging_rate,
+            loss_dict = self.loss_function( self.dataloader,
                                             binning=binning,
                                             weight_by_distance=weight_by_distance, )
             # divide by batches inplace
@@ -188,135 +200,125 @@ class Fitter_AE:
         }
         torch.save(checkpoint, self.checkpoint)
 
-    # TODO: calculate norms on max intensity
-    def loss_function(self,
-                      train_iterator,
-                      coef1=0,
-                      coef2=0,
-                      coef3=0,
-                      coef4=0,
-                      coef5=0,
-                      ln_parm=1,
-                      beta=None,
-                      fill_embeddings=False,
-                      minibatch_logging_rate=None,
-                      binning=False,
-                      weight_by_distance=False):
-        """computes the loss function for the training
+    # Loss stuff
+    def _initialize_loss_components(self, train_iterator, coef1, coef2, coef3, coef4):
+        """Initialize loss components and their coefficients"""
+        components = {
+            'weighted_ln': (Weighted_LN_loss(coef=coef1, channels=self.num_fits).to(self.device) if coef1 > 0 else None),
+            'contrastive': (ContrastiveLoss(coef2).to(self.device) if coef2 > 0 else None),
+            'divergence': (DivergenceLoss(train_iterator.batch_size, coef3).to(self.device) if coef3 > 0 else None),
+            'sparse_max': (Sparse_Max_Loss(min_threshold=self.learning_rate, channels=self.num_fits, coef=coef4).to(self.device) if coef4 > 0 else None)
+        }
+        return components
 
-        Args:
-            train_iterator (torch.Dataloader): dataloader for the training
-            coef1 (float, optional): Ln hyperparameter. Defaults to 0.
-            coef2 (float, optional): hyperparameter for contrastive loss. Defaults to 0.
-            coef3 (float, optional): hyperparameter for divergence loss. Defaults to 0.
-            ln_parm (float, optional): order of the regularization. Defaults to 1.
-            beta (float, optional): beta value for VAE. Defaults to None.
-
-        Returns:
-            _type_: _description_
-        """
-        # set the train mode
-        self.Fitter.train()
-
-        # loss of the epoch
-        loss_dict = {'weighted_ln_loss': 0,
-                    #  'contras_loss': 0,
-                    #  'maxi_loss': 0,
-                     'mse_loss': 0,
-                     'train_loss': 0,
-                     'sparse_max_loss': 0,
-                     'l2_batchwise_loss': 0,
-                     }
-        weighted_ln_ = Weighted_LN_loss(coef=coef1,channels=self.num_fits).to(self.device)
-        con_l = ContrastiveLoss(coef2).to(self.device)
-        maxi_ = DivergenceLoss(train_iterator.batch_size, coef3).to(self.device)
-        sparse_max = Sparse_Max_Loss(min_threshold=self.learning_rate,
-                                        channels=self.num_fits, 
-                                        coef=coef4).to(self.device)
+    def _process_batch_binning(self, x, predicted_x, idx, weight_by_distance=False):
+        """Process batch with binning and optional distance weighting"""
+        x = list(torch.split(x, self.dataloader_sampler.num_neighbors))
+        predicted_x = list(torch.split(predicted_x, self.dataloader_sampler.num_neighbors))
         
-        for i,(idx,x) in enumerate( tqdm( train_iterator, leave=True, total=len(train_iterator) ) ):
-            # tic = time.time()
+        if not weight_by_distance:
+            x = torch.stack([x_.mean(dim=0) for x_ in x])
+            predicted_x = torch.stack([x_.mean(dim=0) for x_ in predicted_x])
+            return x, predicted_x
+        
+        # Weight by distance logic
+        idx = torch.split(idx, self.dataloader_sampler.num_neighbors)
+        weight_list = []
+        
+        for i_, sample_group in enumerate(idx):
+            p_ind, shp = self.dataloader_sampler._which_particle_shape(sample_group[0])
+            coords = [(int((ind - p_ind) % shp[1]), int((ind - p_ind) / shp[0])) for ind in sample_group]
+            weights = torch.tensor([1] + [1 / (1 + ((coords[0][0]-coord[0])**2 + (coords[0][1]-coord[1])**2)**0.5) 
+                                        for coord in coords[1:]], device=self.device)
+            
+            x[i_] = x[i_] * weights.unsqueeze(-1).unsqueeze(-1)
+            predicted_x[i_] = predicted_x[i_] * weights.unsqueeze(-1).unsqueeze(-1)
+            weight_list.append(weights)
+        
+        weight_sums = torch.stack([w.sum(dim=0) for w in weight_list]).unsqueeze(-1).unsqueeze(-1)
+        x = torch.stack([x_.sum(dim=0) for x_ in x]) / weight_sums
+        predicted_x = torch.stack([x_.sum(dim=0) for x_ in predicted_x]) / weight_sums
+        
+        return x, predicted_x
+
+    def _compute_losses(self, embedding, x, predicted_x, loss_components, coef5):
+        """Compute all loss components"""
+        loss_dict = {
+            'weighted_ln_loss': 0, 'mse_loss': 0, 'train_loss': 0,
+            'sparse_max_loss': 0, 'l2_batchwise_loss': 0
+        }
+        
+        # Compute individual losses
+        losses = {
+            'reg_loss_1': loss_components['weighted_ln'](embedding[:,:,0]) if loss_components['weighted_ln'] else 0,
+            'contras_loss': loss_components['contrastive'](embedding[:,:,0]) if loss_components['contrastive'] else 0,
+            'maxi_loss': loss_components['divergence'](embedding[:,:,0]) if loss_components['divergence'] else 0,
+            'sparse_max_loss': loss_components['sparse_max'](embedding[:,:,0]) if loss_components['sparse_max'] else 0,
+        }
+        
+        # L2 batchwise loss
+        if coef5 > 0:
+            losses['l2_loss'] = coef5 * ((embedding[:,:,1]/embedding[:,:,2]).max(dim=0).values - 
+                                        (embedding[:,:,1]/embedding[:,:,2]).min(dim=0).values).mean()
+        else:
+            losses['l2_loss'] = 0
+        
+        # MSE loss
+        mse_loss = F.mse_loss(x, predicted_x, reduction='mean')
+        
+        # Update loss dictionary
+        loss_dict.update({k: v for k, v in losses.items() if v != 0})
+        loss_dict['mse_loss'] = mse_loss.item()
+        
+        # Compute total loss
+        total_loss = mse_loss + losses['reg_loss_1'] + losses['contras_loss'] - losses['maxi_loss'] + losses['l2_loss']
+        loss_dict['train_loss'] = total_loss.item()
+        
+        return total_loss, loss_dict
+
+    def loss_function(self, train_iterator, coef1=0, coef2=0, coef3=0, coef4=0, coef5=0,
+                     ln_parm=1, beta=None, fill_embeddings=False, minibatch_logging_rate=None,
+                     binning=False, weight_by_distance=False):
+        """Main loss function with modular components"""
+        self.Fitter.train()
+        loss_components = self._initialize_loss_components(train_iterator, coef1, coef2, coef3, coef4)
+        accumulated_loss_dict = {'weighted_ln_loss': 0, 'mse_loss': 0, 'train_loss': 0,
+                               'sparse_max_loss': 0, 'l2_batchwise_loss': 0}
+
+        for i, (idx, x) in enumerate(tqdm(train_iterator, leave=True, total=len(train_iterator))):
             idx = idx.to(self.device).squeeze()
             x = x.to(self.device, dtype=torch.float)
-
-            # update the gradients to zero
+            
             self.optimizer.zero_grad()
-
-            if beta is None: embedding, predicted_x = self.Fitter(x)
-            else: embedding, sd, mn, predicted_x = self.Fitter(x)
             
-            # TODO: weight by euclidean distance to 1st point in the bin
-            # i, idx, x are lists. Each list is the gaussian samples from a batch
+            # Forward pass
+            if beta is None:
+                embedding, predicted_x = self.Fitter(x)
+            else:
+                embedding, sd, mn, predicted_x = self.Fitter(x)
+            
+            # Process binning if needed
             if binning:
-                x = list(torch.split(x, self.dataloader_sampler.num_neighbors)) # Split the batch into groups based on the number of neighbors
-                predicted_x = list(torch.split(predicted_x, self.dataloader_sampler.num_neighbors))
-                
-                if weight_by_distance:
-                    idx = torch.split(idx, self.dataloader_sampler.num_neighbors) # Split the indices into groups based on the number of neighbors
-                    weight_list = []
-                    for i_, sample_group in enumerate(idx):
-                        p_ind, shp = self.dataloader_sampler._which_particle_shape(sample_group[0]) # Determine the particle index and shape for the current sample group
-                        coords = [(int((ind - p_ind) % shp[1]), int((ind - p_ind) / shp[0])) for ind in sample_group] # Calculate the coordinates relative to the first point in the group
-                        weights = torch.tensor([1]+[1 / (1 + ((coords[0][0]-coord[0])**2 + (coords[0][1]-coord[1])**2)**0.5) for coord in coords[1:]], # Calculate weights based on the Euclidean distance to the first point
-                                                device = self.device) 
-                        x[i_] = x[i_]*weights.unsqueeze(-1).unsqueeze(-1)
-                        predicted_x[i_] = predicted_x[i_]*weights.unsqueeze(-1).unsqueeze(-1)  
-                        weight_list.append(weights)
-                    weight_sums = torch.stack([weight_.sum(dim=0) for weight_ in weight_list]).unsqueeze(-1).unsqueeze(-1) # Sum the weights in each group and stack them into a new batch
-                    x = torch.stack([x_.sum(dim=0) for x_ in x])/weight_sums # Sum the tensors in each group and stack them into a new batch
-                    predicted_x = torch.stack([x_.sum(dim=0) for x_ in predicted_x])/weight_sums
-                else:        
-                    x = torch.stack([x_.mean(dim=0) for x_ in x]) # Sum the tensors in each group and stack them into a new batch
-                    predicted_x = torch.stack([x_.mean(dim=0) for x_ in predicted_x])
-                    # print('')
-
-            if coef1 > 0: 
-                reg_loss_1 = weighted_ln_(embedding[:,:,0])
-                loss_dict['weighted_ln_loss']+=reg_loss_1
-            else: reg_loss_1 = 0
-
-            if coef2 > 0: 
-                contras_loss = con_l(embedding[:,:,0])
-                loss_dict['contras_loss']+=contras_loss
-            else: contras_loss = 0
-                
-            if coef3 > 0: 
-                maxi_loss = maxi_(embedding[:,:,0])
-                loss_dict['maxi_loss']+=maxi_loss
-            else: maxi_loss = 0
+                x, predicted_x = self._process_batch_binning(x, predicted_x, idx, weight_by_distance)
             
-            if coef4 > 0: # sparse_max_loss
-                sparse_max_loss = sparse_max(embedding[:,:,0])
-                loss_dict['sparse_max_loss']+=sparse_max_loss
-            else: sparse_max_loss = 0
+            # Compute losses
+            loss, batch_loss_dict = self._compute_losses(embedding, x, predicted_x, loss_components, coef5)
             
-            if coef5 > 0: # set so the variation in x < fwhm, but the smaller the better.
-                l2_loss = coef5*( (embedding[:,:,1]/embedding[:,:,2]).max(dim=0).values - \
-                                  (embedding[:,:,1]/embedding[:,:,2]).min(dim=0).values ).mean()
-                loss_dict['l2_batchwise_loss'] += l2_loss
-                
-            else: l2_loss = 0
+            # Update accumulated losses
+            for k in accumulated_loss_dict:
+                if k in batch_loss_dict:
+                    accumulated_loss_dict[k] += batch_loss_dict[k]
             
-            loss = F.mse_loss(x, predicted_x, reduction='mean');
-            
-            loss_dict['mse_loss'] += loss.item()
-            
-            loss = loss + reg_loss_1 + contras_loss - maxi_loss + l2_loss
-            loss_dict['train_loss'] += loss.item()
-
-            # backward pass
+            # Backward pass and optimization
             loss.backward()
-
-            # update the weights
             self.optimizer.step()
             
-            # fill embedding if the correct epoch
+            # Handle embeddings and logging
             if fill_embeddings:
-                sorted, indices = torch.sort(idx)
-                sorted = sorted.detach().numpy()
-                self.embedding[sorted] = embedding[indices].cpu().detach().numpy()
-                # print('\tt', abs(tic-toc)) # 2.7684452533721924
-            if minibatch_logging_rate is not None: 
-                if i%minibatch_logging_rate==0: wandb.log({k: v/(i+1) for k,v in loss_dict.items()})
+                sorted_idx, indices = torch.sort(idx)
+                self.embedding[sorted_idx.detach().numpy()] = embedding[indices].cpu().detach().numpy()
+                
+            if minibatch_logging_rate and i % minibatch_logging_rate == 0:
+                wandb.log({k: v/(i+1) for k, v in accumulated_loss_dict.items()})
 
-        return loss_dict
+        return accumulated_loss_dict
