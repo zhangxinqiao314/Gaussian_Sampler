fdd4913149abb820e10b511905d951c10c04dae1
zhangxinqiao314 <zhang.xinqiao314@gmail.com>
2026-01-23 11:44:27 -0500

    profiling func

--- Gaussian_Sampler/utils.py
+++ Gaussian_Sampler/utils.py
@@ -1,2 +1,13 @@
 import numpy as np
+import time
+
+# Profiling decorator
+def profile(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        print(f"{func.__name__} took {end_time - start_time:.4f} seconds")
+        return result
+    return wrapper
 
